b94a719989bd17fff9b64fcdc67b6d684653e3ff
Željko Beratović <zeljkoberatovic084@gmail.com>
2025-07-28 13:57:40 +0200

    feat: implementiran lazy loading za sve ključne rute ( bibliotekari, view/edit bibliotekar) kako bi se komponente učitavale po potrebi i poboljšale performanse

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -1,24 +1,28 @@
 import { Route } from '@angular/router';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { Librarians } from './pages/librarians/librarians/librarians';
-import { NewLibrarian } from './pages/librarians/new-librarian/new-librarian';
-
-
-// import other components…
+ 
 
 export const routes: Route[] = [
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
-  { path: 'dashboard', component: DashboardComponent },
-  { path: 'bibliotekari', component: Librarians },
-  { path: 'noviBibliotekar', component: NewLibrarian },
-  { path: 'bibliotekari/:id', loadComponent: () => import('./pages/librarians/view-librarian/view-librarian').then(m => m.ViewLibrarian) },
-{
-  path: 'bibliotekari/:id/izmjena',
-  loadComponent: () =>
-    import('./pages/librarians/edit-librarian/edit-librarian').then(m => m.EditLibrarian)
-}
-
- 
- 
-  // …others…
+  {
+    path: 'dashboard',
+    loadComponent: () => import('./pages/dashboard/dashboard.component').then(m => m.DashboardComponent)
+  },
+  {
+    path: 'bibliotekari',
+    loadComponent: () => import('./pages/librarians/librarians/librarians').then(m => m.Librarians)
+  },
+  {
+    path: 'noviBibliotekar',
+    loadComponent: () => import('./pages/librarians/new-librarian/new-librarian').then(m => m.NewLibrarian)
+  },
+  {
+    path: 'bibliotekari/:id',
+    loadComponent: () => import('./pages/librarians/view-librarian/view-librarian').then(m => m.ViewLibrarian)
+  },
+  {
+    path: 'bibliotekari/:id/izmjena',
+    loadComponent: () => import('./pages/librarians/edit-librarian/edit-librarian').then(m => m.EditLibrarian)
+  }
 ];
+
