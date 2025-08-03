82c801ccb5d7548aa6acbc7cacf64346e48105ca
Željko Beratović <zeljkoberatovic084@gmail.com>
2025-08-03 19:04:56 +0200

    chore(structure): premješten auth-interceptor u interceptors folder

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import {  withInterceptors } from '@angular/common/http';
 import { App }      from './app/app';
 import { routes }   from './app/routes';
 import { provideHttpClient, withFetch } from '@angular/common/http';
-import { authInterceptor } from './app/auth-interceptor';
+import { authInterceptor } from './app/interceptors/auth-interceptor';
 
 bootstrapApplication(App, {
   providers: [
